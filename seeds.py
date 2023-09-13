c739bcbb2eb2944672f70e9d6e5b361edbd4daa0
Kamente <justinm48000@gmail.com>
2023-09-13 20:43:52 +0300

    made corrections on the seeds.py

--- seeds.py
+++ seeds.py
@@ -49,40 +49,39 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Playlist, PlaylistSongs, User, UserPlaylists, Artist, Album, Song, Genre 
+from models import Playlist, PlaylistSongs, User, UserPlaylists, Artist, Album, Song, Genre
 
 DATABASE_URI = 'sqlite:///music.db'
-engine = create_engine(DATABASE_URI, echo = True)
-Session = sessionmaker
-session = Session
+engine = create_engine(DATABASE_URI, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-def create_data ():
-    artist1 = Artist(artist_name = "Gabzy")
-    artist2 = Artist(artist_name = "Khalid")
-    
-    album1 = Album(album_title = "MasterMind", artist = artist1)
-    album2 = Album(album_title = "Summers", artist = artist2)
-    
-    
-    genre1 = Genre(genre_name = "HipHop")
-    genre2 = Genre(genre_name = "Pop")
-    
-    user1 = User(user_name = "Kamente")
-    user2 = User(user_name = "Justin")
-    
-    playlist1 = Playlist(playlist_name = "MyPlaylist")
-    
-    song1 = Song(song_title = "Come Over", song_duration = 200, album=album1, artist = artist1, genre = genre1)
-    song1 = Song(song_title = "Eastside", song_duration = 270, album=album2, artist = artist2, genre = genre2)
+def create_data():
+    artist1 = Artist(artist_name="Gabzy")
+    artist2 = Artist(artist_name="Khalid")
+
+    album1 = Album(album_title="MasterMind", artist=artist1)
+    album2 = Album(album_title="Summers", artist=artist2)
+
+    genre1 = Genre(genre_name="HipHop")
+    genre2 = Genre(genre_name="Pop")
+
+    user1 = User(user_name="Kamente")
+    user2 = User(user_name="Justin")
+
+    playlist1 = Playlist(playlist_name="MyPlaylist")
+
+    song1 = Song(song_title="Come Over", song_duration=200, album=album1, artist=artist1, genre=genre1)
+    song2 = Song(song_title="Eastside", song_duration=270, album=album2, artist=artist2, genre=genre2)
 
     playlist1.songs.append(song1)
     playlist1.songs.append(song2)
-    
+
     user1.playlists.append(playlist1)
     user2.playlists.append(playlist1)
-    
-    session.add_all(user1, user2, artist1, artist2, genre1, genre2, album1, album2, song1, song2, playlist1)
+
+    session.add_all([user1, user2, artist1, artist2, genre1, genre2, album1, album2, song1, song2, playlist1])
     session.commit()
-    
-    if __name__ == '__main__':
-        create_data()
+
+if __name__ == '__main__':
+    create_data()
